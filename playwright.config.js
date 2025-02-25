259421d2772ce327bd9a249dfcea304b0655d1d8
Eric Donnelly <eric@donnellydev.ca>
2025-02-24 22:47:14 -0500

    updated tests to better match with expectations - properly runs only if html file exists and is setup properly

--- playwright.config.js
+++ playwright.config.js
@@ -4,20 +4,36 @@ const {
   "liveServer.settings.port": liveServerPort,
 } = require("./.vscode/settings.json");
 const mainPageUrl = `http://localhost:${liveServerPort}/`;
+
 module.exports = defineConfig({
   testDir: "./tests",
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   reporter: [["list"], ["html", { open: "never" }]],
+
   use: {
     baseURL: mainPageUrl,
     trace: "on-first-retry",
   },
+
   projects: [
     {
-      name: "chromium",
+      name: "html-structure",
+      testMatch: /html-validate\.spec\.js/,
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "part1-tests",
+      testMatch: /tests-p1\.spec\.js/,
+      dependencies: ["html-structure"],
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "part2-tests",
+      testMatch: /tests-p2\.spec\.js/,
+      dependencies: ["html-structure"],
       use: { ...devices["Desktop Chrome"] },
     },
   ],
